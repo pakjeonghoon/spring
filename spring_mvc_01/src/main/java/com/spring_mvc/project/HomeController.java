e9f39576410c4fd8f73ef3bd77e19a764d34cb43
pakjeonghoon <gsm04283@gmail.com>
2022-12-12 09:29:26 +0900

    initial upload

---
+++ spring_mvc_01/src/main/java/com/spring_mvc/project/HomeController.java
@@ -0,0 +1,40 @@
+package com.spring_mvc.project;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );  //바인딩
+		
+		return "home"; //기본으로 views 폴더의 home.jsp
+		// 컨트롤러에서 반환하는 문자열은 View 페이지(jsp)의 이름
+	}
+	
+}
